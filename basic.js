80bb7ea2253154b31324fbe712b59fb5976791ac
maruf-hussain <engmarufhussain@gmail.com>
2024-05-01 16:09:27 +0600

    Data Typs Complete

---
+++ basic.js
@@ -0,0 +1,56 @@
+// Variables
+// Data types
+// Functions
+// Loops
+// Conditions
+// Arrays
+// Objects
+// DOM manipulation
+// Asynchronous programming
+// Functional programming
+
+
+// 1.Variable.*******************************************************************
+// var a = 10;
+// var b = 20;
+// var c = a + b;
+// console.log(c);
+
+
+// let Maruf = 28;
+// let Sobuj = 26;
+// let total = Maruf + Sobuj;
+// console.log(total);
+
+// let totalTaka = 7800;
+// let Computer = 2500;
+// let Phone = 4000;
+// let cashDebit = Computer + Phone;
+// let cashCedit = totalTaka - cashDebit;
+// console.log(cashCedit);
+
+
+// 2.Data Type..**********************************************************************
+// Number........
+// let lentgh = 45;
+// let whidth = 10;
+
+// // String........
+// let myName = "Maruf";
+// let favoriteColor = "Orange";
+
+// // Boleans...........
+// let x = true;
+// let y = false;
+
+// // Objects..............
+// const personMaruf = {village:"Begunbari", Upazila:"companigong", District:"Noakhali"};
+
+// // Array and Objects...........
+// const company = ["Apple", "Google", "Samsung"];
+
+// // Date Objects.........
+// const date = newDate("2024-05-01");
+
+
+// 3.Functions.***********************************************************************
---
+++ es6.js
@@ -0,0 +1,11 @@
+// Arrow functions
+// Template literals
+// Shorthand property names
+// Parameter defaults
+// Rest/spread
+// ESModules
+// Ternaries
+// Array methods like map and filter
+// Nullish coalescing operator
+// Optional chaining
+// Promises and async/await
