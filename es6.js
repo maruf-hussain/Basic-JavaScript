f956fad87d6de40e52e0d9a5d0ac313c867e001d
maruf-hussain <engmarufhussain@gmail.com>
2024-05-05 02:23:38 +0600

    arrow function and desturing

--- es6.js
+++ es6.js
@@ -9,3 +9,40 @@
 // Nullish coalescing operator
 // Optional chaining
 // Promises and async/await
+
+// 1.Arrow Functions**********************************************************
+// const add = (num1, num2) =>{
+//     return num1 + num2;
+// }
+// const sum = add(25,50);
+// console.log(sum);
+// ......................................
+// const equal = (a,b,c) =>{
+//     const add = a + b + c;
+//     const multipy = a * b * c;
+//     const result = add + multipy;
+//     return result;
+// }
+
+// const total = equal(2,5,10);
+// console.log(total);
+
+// 2.Template Literals*******************************************************
+// const email =`marufhussainns
+// emmaruf
+// sobujhasan`;
+// console.log(email);
+
+// 3.Objects Destructure****************************************************
+// const Artist = {
+//     name:'Maruf',
+//     Age: 28,
+//     Occupation: 'Islamic Song',
+//     FavoriteWork:'Tourist',
+//     Address:'chanduria,kolaroa,satkhira'
+// };
+// const {name,Age,Occupation} = Artist;
+// console.log(name,Age,Occupation);
+// .........................................................
+
+// 4.Array Destructure*********************************************
